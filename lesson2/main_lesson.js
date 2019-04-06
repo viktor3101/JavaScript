4a0dbfe53c60ab72f5c3cb6bb9fdebbd0791978f
viktor3101 <viktorgusakivskii@gmail.com>
2019-04-06 12:54:22 +0300

    Add lesson2 classes

---
+++ lesson2/main_lesson.js
@@ -0,0 +1,30 @@
+;
+(function () {
+    var sum = function( a, b){
+        return a + b;
+    }
+    var str = sum("asdasdasd","asdasdadad");
+    console.log(str);
+       
+
+    var a = 10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} () );
